15f22abbe4738f33d7f473f0fd95356ccd08427a
arshia <arshia.sedghi011@gmail.com>
2025-04-25 19:28:46 +0330

    develope TextFile Manager 2

--- I_S/src/main/java/com/example/i_s/managers/TextFile_Manager.java
+++ I_S/src/main/java/com/example/i_s/managers/TextFile_Manager.java
@@ -1,14 +1,18 @@
 package com.example.i_s.managers;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class TextFile_Manager {
     private String FileName;
     private String FilePath;
     private int CRow;
+    private String Data[];
+
     public TextFile_Manager(String FileName) {
         this.FileName = FileName;
         CRow=0;
@@ -36,6 +40,45 @@ public class TextFile_Manager {
         fw.close();
         CRow++;
     }
+
+    private void File2Array() throws FileNotFoundException{
+
+        File file= new File(FilePath);
+        Scanner fw=new Scanner(file);
+        Data=new String[1000];
+        CRow=0;
+    
+        while (fw.hasNextLine()){
+            Data[CRow]=fw.nextLine();
+            CRow++;
+        }
+        fw.close();
+    }
+
+    private void Array2File() throws IOException{
+
+        File file= new File(FilePath);
+        FileWriter fw = new FileWriter(file,true);
+        for (int i=0; i<CRow; i++){
+            fw.append(Data[i]+"\n");
+        }
+        fw.close();
+    }
+
+
+
+    public void delete_Rows(int a) throws IOException{
+
+        File file=new File(FilePath);
+        FileWriter fw = new FileWriter(file,true);
+        for (int i=0; i<CRow ; i++){
+            if(i!=a){
+                fw.append(Data[i]+"\n");
+            }
+        }
+        fw.close();
+    }
+
 
 
 
