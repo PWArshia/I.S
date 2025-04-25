0bb7b2ddf825c290b05ad70046b8f26d8ac65023
kamy <kamyar1385kamyaresmaeilpour@gmail.com>
2025-04-25 17:15:00 +0330

    Creat first UI

--- I_S/src/main/java/com/example/i_s/HelloController.java
+++ I_S/src/main/java/com/example/i_s/HelloController.java
@@ -1,14 +1,37 @@
 package com.example.i_s;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+    Stage MemberWindow=new Stage();
+    Stage RoomWindow=new Stage();
+
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void MembersCo(ActionEvent actionEvent) throws IOException {
+        FXMLLoader MembersLoader = new FXMLLoader(getClass().getResource("Members.fxml"));
+        Scene MembersScene = new Scene(MembersLoader.load(),600,400);
+        MemberWindow.setScene(MembersScene);
+        MemberWindow.setTitle("Members");
+        MemberWindow.show();
+    }
+
+    public void RoomsCo(ActionEvent actionEvent) throws IOException {
+        FXMLLoader RoomsLoader = new FXMLLoader(getClass().getResource("Rooms.fxml"));
+        Scene RoomsScene = new Scene(RoomsLoader.load(),600,400);
+        RoomWindow.setScene(RoomsScene);
+        RoomWindow.setTitle("Rooms");
+        RoomWindow.show();
+
     }
 }
