12e973734a3d3d33052bcf5b830635f8e3f36890
arshia sedghi <108299077+PWArshia@users.noreply.github.com>
2025-04-29 11:23:12 +0330

    Update DessertManager.java
    Debug

--- I_S/src/main/java/EntityManagers/DessertManager.java
+++ I_S/src/main/java/EntityManagers/DessertManager.java
@@ -37,9 +37,9 @@ public class DessertManager {
 
             int ID=Integer.parseInt(B[0]);
             String Name=B[1];
-            int price=Integer.parseInt(B[2]);
+            double price=Double.parseDouble(B[2]);
             int No=Integer.parseInt(B[4]);
-            int BuyPrice=Integer.parseInt(B[3]);
+            double BuyPrice=Double.parseDouble(B[3]);
             dessert[i].SetId(ID);
             dessert[i].SetName(Name);
             dessert[i].SetPrice(price);
